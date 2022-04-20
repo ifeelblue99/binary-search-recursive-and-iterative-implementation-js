97bbb72afe3dd771aa89874fcf734d3a4e2920ea
Baran Cakirbey <96394372+ifeelblue99@users.noreply.github.com>
2022-04-20 11:52:17 +0300

    major bug fixed

--- example.js
+++ example.js
@@ -24,26 +24,25 @@ function binarySearchRecursive(arr, target, start, end){
 // driver code
 //console.log(binarySearch(dataArray, 18))
 
-/* / iterative has a major bug to be fixed as soon as posible
+const dataArray = [1, 2, 4, 5, 6, 8, 10 ,11 ,12 ,15 ,16 ,17, 18 ,19 ,20]
+
 function binarySearchIterative(arr, target){
  
     let start = 0
     let end = arr.length-1
     
-    while (start<end) {
-        let middleIndex = Math.floor((start+end)/2)
+    while (end>=start) {
+        let middleIndex = Math.round((end-start)/2) + start
         if(arr[middleIndex]===target){
             return `found at index: ${middleIndex}`
         }
-        else if (arr[middleIndex]>target) {
-            end = middleIndex-1     
-        }else{
-            start = middleIndex+1
+        else if (arr[middleIndex]<target) {
+            start = middleIndex+1     
+        }else if(arr[middleIndex]>target){
+            end = middleIndex-1
         }   
     }
-    return "no valid data found"
+    return "no valid data exists"
 }
-
 // driver code
-console.log(binarySearchIterative(dataArray, 22))
-*/
+console.log(binarySearchIterative(dataArray, 20))
