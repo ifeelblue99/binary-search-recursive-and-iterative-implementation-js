49a368a8ebb22dcb31d245381e4feecb914b5731
Baran Cakirbey <96394372+ifeelblue99@users.noreply.github.com>
2022-04-30 15:34:45 +0300

    bug fixed

--- example.js
+++ example.js
@@ -5,44 +5,38 @@
 const dataArray = [1, 2, 4, 5, 6, 8, 10 ,11 ,12 ,15 ,16 ,17, 18 ,19 ,20]
 
 // recursive 
-function binarySearchRecursive(arr, target, start, end){
-    
+function binarySearchRecursive(arr, target, start = 0, end = arr.length-1){
     if(start>end) return false
 
-    let middleIndex = Math.floor((arr.length-1)/2)
+    const middleIndex = Math.floor((end-start)/2) + start
     
-    if(arr[middleIndex]===target) return true
+    if(arr[middleIndex]===target) return middleIndex
 
-
-    if (arr[middleIndex]>target) {
-        return binarySearch(arr, target, start, middleIndex-1)        
+    else if (arr[middleIndex]>target) {
+        return binarySearchRecursive(arr, target, start, middleIndex-1)        
     }else{
-        return binarySearch(arr, target, middleIndex+1, end)        
+        return binarySearchRecursive(arr, target, middleIndex+1, end)        
     }
 }
-
 // driver code
-//console.log(binarySearch(dataArray, 18))
-
-const dataArray = [1, 2, 4, 5, 6, 8, 10 ,11 ,12 ,15 ,16 ,17, 18 ,19 ,20]
+console.log("target found at.",binarySearchRecursive(dataArray, 15))
 
 function binarySearchIterative(arr, target){
- 
     let start = 0
     let end = arr.length-1
     
     while (end>=start) {
-        let middleIndex = Math.round((end-start)/2) + start
+        let middleIndex = Math.floor((end-start)/2) + start
         if(arr[middleIndex]===target){
             return `found at index: ${middleIndex}`
         }
         else if (arr[middleIndex]<target) {
             start = middleIndex+1     
-        }else if(arr[middleIndex]>target){
+        }else{
             end = middleIndex-1
         }   
     }
     return "no valid data exists"
 }
 // driver code
-console.log(binarySearchIterative(dataArray, 20))
+console.log(binarySearchIterative(dataArray, 15))
